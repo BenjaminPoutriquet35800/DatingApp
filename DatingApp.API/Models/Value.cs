92e315b98f08067e9f8d2315345bdc46e957d4d1
BenjaminPoutriquet35800 <benjaminpoutriquet@hotmail.fr>
2018-09-16 15:34:41 +0200

    Initial Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Models
+{
+    using System;
+
+    public class Value
+    {
+        public Int32 Id { get; set; }
+        public String Name { get; set; }     
+    }
+}
