0600744ec2656bf73c70cae65ff41aa3bed4cb29
BenjaminPoutriquet35800 <benjaminpoutriquet@hotmail.fr>
2018-10-07 18:58:01 +0200

    End Section 9

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DatingApp.API.Models
 {
@@ -6,9 +8,18 @@ namespace DatingApp.API.Models
     {
         public Int32 Id { get; set; }
         public String Username { get; set; }
-
         public Byte[] PasswordHash { get; set; }
-
         public Byte[] PasswordSalt { get; set; }
+        public String Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public String KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public String Introduction { get; set; }
+        public String LookingFor { get; set; }
+        public String Interests { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
@@ -19,6 +13,7 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
@@ -36,36 +31,46 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                    .AddJsonOptions(options =>
+                    {
+                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    });
             services.AddCors();
+            services.AddAutoMapper();
+
+            services.AddTransient<Seed>();
 
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     .AddJwtBearer(options =>
                     {
                         options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
                         {
-                            ValidateIssuer = true,
-                            
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
                             IssuerSigningKey = GetSymmetricSecurityKey()
                         };
                     });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seed)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
 
-            app.UseExceptionHandler(builder => {
-                builder.Run(async context => {
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
                     context.Response.StatusCode = (Int32)HttpStatusCode.InternalServerError;
 
                     var error = context.Features.Get<IExceptionHandlerFeature>();
 
-                    if(error != null)
+                    if (error != null)
                     {
                         context.Response.AddApplicationError(error.Error.Message);
                         await context.Response.WriteAsync(error.Error.Message);
@@ -73,6 +78,7 @@ namespace DatingApp.API
                 });
             });
 
+            //seed.SeedUsers();
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyHeader()
                               .AllowAnyOrigin()
