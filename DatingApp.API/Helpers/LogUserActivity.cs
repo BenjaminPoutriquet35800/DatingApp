f53becb047c5e17d97339e0d6b9cc6077cf3b01f
Poutriquet <benjaminpoutriquet@hotmail.fr>
2020-03-21 12:15:28 +0100

    Enf of Section 13

---
+++ DatingApp.API/Helpers/LogUserActivity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+using DatingApp.API.Data;
+
+namespace DatingApp.API.Helpers
+{
+    public class LogUserActivity : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var resultContext = await next();
+
+            var userId = int.Parse(resultContext.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var repository = resultContext.HttpContext.RequestServices.GetService<IDatingRepository>();
+
+            var user = await repository.GetUser(userId);
+
+            user.LastActive = DateTime.Now;
+
+            await repository.SaveAll();
+        }
+    }
+}
